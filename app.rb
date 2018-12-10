1fb45887fa517a7918deace1fbfd839338bd247d
Manon Pilaud <mandyypilaud@gmail.com>
2018-12-09 20:50:57 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+     "My name is Mandy"
+  end
+  get '/hometown' do
+    "My hometown is Grenoble"
+  end
+  get '/favorite-song' do
+    "My favorite song is _"
+  end
 end
